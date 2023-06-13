251ff39eef35cb42aabc93fc27d5851c9577545c
Denis Tadzhibaev <incinerator88@mail.ru>
2023-06-13 21:29:56 +0300

    feat: add http_methods.py

---
+++ src/http_methods.py
@@ -0,0 +1,46 @@
+import allure
+import requests
+
+
+class MyRequests:
+    @staticmethod
+    def post(url: str, data: dict = None, headers: dict = None, cookies: dict = None):
+        with allure.step(f"POST request to URL '{url}'"):
+            return MyRequests._send(url, data, headers, cookies, "POST")
+
+    @staticmethod
+    def get(url: str, data: dict = None, headers: dict = None, cookies: dict = None):
+        with allure.step(f"GET request to URL '{url}'"):
+            return MyRequests._send(url, data, headers, cookies, "GET")
+
+    @staticmethod
+    def put(url: str, data: dict = None, headers: dict = None, cookies: dict = None):
+        with allure.step(f"PUT request to URL '{url}'"):
+            return MyRequests._send(url, data, headers, cookies, "PUT")
+
+    @staticmethod
+    def delete(url: str, data: dict = None, headers: dict = None, cookies: dict = None):
+        with allure.step(f"DELETE request to URL '{url}'"):
+            return MyRequests._send(url, data, headers, cookies, "DELETE")
+
+    @staticmethod
+    def _send(url: str, data: dict, headers: dict, cookies: dict, method: str):
+        url = f"""{BASE_URL}{url}"""
+
+        if headers is None:
+            headers = {}
+        if cookies is None:
+            cookies = {}
+
+        if method == "GET":
+            response = requests.get(url, params=data, headers=headers, cookies=cookies)
+        elif method == "POST":
+            response = requests.post(url, data=data, headers=headers, cookies=cookies)
+        elif method == "PUT":
+            response = requests.put(url, data=data, headers=headers, cookies=cookies)
+        elif method == "DELETE":
+            response = requests.delete(url, data=data, headers=headers, cookies=cookies)
+        else:
+            raise Exception(f"""Bad method '{method}' was received""")
+
+        return response
