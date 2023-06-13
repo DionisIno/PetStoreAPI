9033c727e20958895bb28aa399453cf03ffec59a
Denis Tadzhibaev <incinerator88@mail.ru>
2023-06-14 01:37:20 +0300

    feat: add conftest.py

---
+++ conftest.py
@@ -0,0 +1,30 @@
+import os
+import shutil
+import allure
+import pytest
+
+
+@allure.description("Function to clear the contents of the logs folder. Return: empty log folder")
+@allure.step("Deleting all contents of the logs folder")
+def clear_logs_directory():
+
+    with allure.step("Getting the absolute path to the current file"):
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+
+    with allure.step("Forming the path to the logs folder"):
+        logs_dir = os.path.join(current_dir, 'logs')
+
+    with allure.step("Checking if the logs folder exists"):
+        if os.path.exists(logs_dir):
+            # Deleting all content in the logs folder
+            shutil.rmtree(logs_dir)
+
+            # Create an empty logs folder
+            os.makedirs(logs_dir)
+
+
+@pytest.hookimpl(tryfirst=True)
+@allure.description("Pytest_sessionstart hook to call a function before starting a session")
+@allure.step("Calling a function to clear the contents of the logs folder before starting a session")
+def pytest_sessionstart(session):
+    clear_logs_directory()
