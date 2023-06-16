52a0a73983453b47daf4dd0c8879ab94e0c92e92
Denis Tadzhibaev <incinerator88@mail.ru>
2023-06-17 01:31:06 +0300

    fix: fix assertions.py

--- src/assertions.py
+++ src/assertions.py
@@ -6,7 +6,7 @@ class Assertions:
     @staticmethod
     def assert_code_status(response: Response, expected_status_code):
         actual_status_code = response.status_code
-        assert actual_status_code == expected_status_code or actual_status_code == 404, \
+        assert actual_status_code == expected_status_code, \
             f"Unexpected status code. Expected: {expected_status_code}. Actual: {actual_status_code}"
 
     @staticmethod
