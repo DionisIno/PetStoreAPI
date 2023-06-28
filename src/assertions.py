62844cd945b8932af0c3660557a8ecd2b72cd1ae
Elena <peregovorun@gmail.com>
2023-06-28 22:40:54 +0300

    test_store_alena

--- src/assertions.py
+++ src/assertions.py
@@ -4,7 +4,7 @@ from requests import Response
 
 class Assertions:
     @staticmethod
-    def assert_code_status(response: Response, expected_status_code):
+    def assert_code_status(response: Response, expected_status_code: object) -> object:
         actual_status_code = response.status_code
         assert actual_status_code == expected_status_code, \
             f"Unexpected status code. Expected: {expected_status_code}. Actual: {actual_status_code}"
