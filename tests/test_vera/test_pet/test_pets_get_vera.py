f1a29e0b5d4b0371cd23b393ccd9bc6f90f136cb
verafes <verafes@gmail.com>
2023-07-07 16:12:59 -0700

    Update: pets tests&data: verafes

--- tests/test_vera/test_pet/test_pets_get_vera.py
+++ tests/test_vera/test_pet/test_pets_get_vera.py
@@ -1,16 +1,16 @@
 import allure
 import pytest
 
-
 from src.assertions import Assertions
 from src.http_methods import MyRequests
 from data.data_pet import get_pet_by_status
 from data.status_code import StatusCode
-from data.urls import PetUrls, BASE_URL
-from tests.test_vera.data.data_pets import expected_pet_keys
+from data.urls import PetUrls
 from tests.test_vera.conf import headers, pet_data_set
+from tests.test_vera.data.data_pets import ExpectedPetsResult as PET
 
 
+@allure.epic('Testing GET method')
 class TestPet:
     status_code = StatusCode()
     link = PetUrls
@@ -23,7 +23,10 @@ class TestPet:
         and checks the status code of the response
         :param for status: "available", "pending",  "sold"
         """
-        response = MyRequests.get(self.link.BY_STATUS, status)
+        response = MyRequests.get(
+            self.link.BY_STATUS,
+            status
+        )
         Assertions.assert_code_status(response, self.status_code.STATUS_CODE_200)
 
     @allure.title("Find pets by status > Expected number of pets for each status")
@@ -33,7 +36,10 @@ class TestPet:
         The test checks if the response contains the expected number of pets for each status
         :param for status: "available", "pending",  "sold"
         """
-        response = MyRequests.get(self.link.BY_STATUS, data=status)
+        response = MyRequests.get(
+            self.link.BY_STATUS,
+            data=status
+        )
         data = response.json()
         assert isinstance(data, list)
         assert all(pet['status'] == status['status'] for pet in data)
@@ -42,46 +48,61 @@ class TestPet:
     @pytest.mark.parametrize("status", get_pet_by_status)
     def test_response_content_type(self, status):
         """ The test checks if the response content type is JSON """
-        response = MyRequests.get(self.link.BY_STATUS, data=status)
+        response = MyRequests.get(
+            self.link.BY_STATUS,
+            data=status
+        )
         Assertions.assert_response_has_be_json(response)
 
     @allure.title("Find pets by status > Response headers contain the necessary fields")
     @pytest.mark.parametrize("status", get_pet_by_status)
     def test_response_headers(self, status):
-        """ The test checks if the response cheaders contain the necessary fields"""
-        get_pet_by_status_headers = ['access-control-allow-headers', 'access-control-allow-methods',
-                                     'access-control-allow-origin',
-                                     'content-type', 'content-type', 'date', 'server']
-        response = MyRequests.get(self.link.BY_STATUS, data=status)
+        """ The test checks if the response headers contain the necessary fields"""
+        response = MyRequests.get(
+            self.link.BY_STATUS,
+            data=status
+        )
         for header, value in response.headers.items():
             print(f"{status} - {header}: {value}")
-            if header in get_pet_by_status_headers:
+            if header in PET.get_pet_by_status_headers:
                 assert header in response.headers, f"Expected header '{header}' not found"
-                assert value == get_pet_by_status_headers[
-                    header], f"Expected value '{get_pet_by_status_headers[header]}' for header '{header}'"
+                assert value == PET.get_pet_by_status_headers[
+                    header], f"Expected value '{PET.get_pet_by_status_headers[header]}' for header '{header}'"
 
-    @allure.title("Create pet > Expected pet keys in response")
-    def test_add_pet_keys_in_response(self, headers, pet_data_set):
-        """ The test checks "id" in json response for the created pet """
-        response = MyRequests.post(self.link.PET, data=pet_data_set, headers=headers)
-        Assertions.assert_response_has_be_json(response)
-        Assertions.assert_json_has_keys(response, expected_pet_keys)
-
-    @allure.title("Create pet > Expected pet name in response")
-    def test_add_pet_name_in_response(self, headers, pet_data_set):
-        """ The test checks pet's name in json response for the created pet """
-        response = MyRequests.post(self.link.PET, data=pet_data_set, headers=headers)
-        Assertions.assert_response_has_be_json(response)
-        Assertions.assert_json_value_by_name(response, 'name', pet_data_set['name'],
-                                             "Incorrect pet name in the response")
-
-    @allure.title("Get pet by ID > Status code is 200")
+    @allure.title("Get a pet by ID > Status code is 200")
     def test_get_pet_by_id(self, headers, pet_data_set):
         """This test to get pet info by ID and ensures status code is 200"""
-        # creating a pet
-        response = MyRequests.post(self.link.PET, data=pet_data_set, headers=headers)
+        #creating a pet
+        response = MyRequests.post(
+            self.link.PET,
+            data=pet_data_set,
+            headers=headers
+        )
         pet_id = response.json()['id']
-        url = self.link.PET + f"/{pet_id}"
-        # getting response
-        response = MyRequests.get(url, headers=headers)
+        #getting status code for the response
+        response = MyRequests.get(
+            self.link.PET + f"/{pet_id}",
+            headers=headers
+        )
         Assertions.assert_code_status(response, self.status_code.STATUS_CODE_200)
+
+    @allure.title("Get a pet by non-existing ID > Status code is 404")
+    def test_get_pet_by_id(self, headers, pet_data_set):
+        """This test to get pet info by non-existing ID and ensures status code is 404"""
+        pet_id = -1
+        response = MyRequests.get(
+            self.link.PET + f"/{pet_id}",
+            headers=headers
+        )
+        Assertions.assert_code_status(response, self.status_code.STATUS_CODE_404)
+
+    @allure.title("Get a pet by non-existing ID > Error message in the response")
+    def test_get_pet_by_id(self, headers, pet_data_set):
+        """This test verifies the response for retrieving pet information by non-existing ID
+        and validates error message in the response."""
+        pet_id = -1
+        response = MyRequests.get(
+            self.link.PET + f"/{pet_id}",
+            headers=headers
+        )
+        Assertions.assert_code_status(response, self.status_code.STATUS_CODE_404)
--- tests/test_vera/test_pet/test_pets_put_verafes.py
+++ tests/test_vera/test_pet/test_pets_put_verafes.py
@@ -3,8 +3,9 @@ import allure
 from src.assertions import Assertions
 from src.http_methods import MyRequests
 from data.status_code import StatusCode
-from data.urls import PetUrls, BASE_URL
+from data.urls import PetUrls
 from tests.test_vera.conf import headers, pet_data_set
+from tests.test_vera.data.data_pets import ExpectedPetsResult as PET
 
 
 class TestUpdatePet:
@@ -16,16 +17,22 @@ class TestUpdatePet:
         """
         The test updates pet's information with valid data and checks the status code of the response
         """
-        pet_data_set["name"] = "new name"
-        response = MyRequests.put(self.link.PET, pet_data_set)
+        pet_data_set["name"] = PET.pet["pet_name"]
+        response = MyRequests.put(
+            self.link.PET,
+            pet_data_set
+        )
         Assertions.assert_code_status(response, self.status_code.STATUS_CODE_200)
-        Assertions.assert_json_value_by_name(response, "name", "new name", "Failed to update the pet's name")
 
     @allure.title("Update pet > Name is updated")
     def test_update_pet_name(self, pet_data_set):
         """
         Test updates the pet's name and checks if it is updated successfully.
         """
-        pet_data_set["name"] = "new name"
-        response = MyRequests.put(self.link.PET, pet_data_set)
-        Assertions.assert_json_value_by_name(response, "name", "new name", "Failed to update the pet's name")
+        pet_data_set["name"] = PET.pet["pet_name"]
+        response = MyRequests.put(
+            self.link.PET,
+            pet_data_set
+        )
+        actual_pet_name = response.json()["name"]
+        Assertions.assert_json_value_by_name(response, "name", actual_pet_name, "Failed to update the pet's name")
